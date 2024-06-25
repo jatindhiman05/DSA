0aff60713c4719ce0626d7c26c11311e08ad8e84
jatinn27 <jatin121dhiman@gmail.com>
2024-06-25 23:31:18 +0530

    optimal

--- 12-BINARY SEARCH TREE/1- Binary Search Tree to Greater Sum Tree/lc_1038.cpp
+++ 12-BINARY SEARCH TREE/1- Binary Search Tree to Greater Sum Tree/lc_1038.cpp
@@ -12,6 +12,7 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// m-1
 class Solution
 {
 public:
@@ -36,6 +37,30 @@ public:
             sum += nodes[i]->val;
             nodes[i]->val = sum;
         }
+
+        return root;
+    }
+};
+
+// m - 2
+class Solution
+{
+public:
+    void solve(TreeNode *root, int &sum)
+    {
+        if (!root)
+            return;
+
+        solve(root->right, sum);
+        sum += root->val;
+        root->val = sum;
+        solve(root->left, sum);
+    }
+
+    TreeNode *bstToGst(TreeNode *root)
+    {
+        int sum = 0;
+        solve(root, sum);
 
         return root;
     }
