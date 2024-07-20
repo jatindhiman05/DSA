fb061d97d9fd4e2abb42f16bd5d25bdbc1a7b85d
jatinn27 <jatin121dhiman@gmail.com>
2024-07-20 09:52:14 +0530

    Find Valid Matrix Given Row and Column Sums

---
+++ 3-2D ARRAYS/10-Find Valid Matrix Given Row and Column Sums/lc_1605.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> restoreMatrix(vector<int> &rowSum, vector<int> &colSum)
+    {
+        vector<vector<int>> result(rowSum.size(), vector<int>(colSum.size()));
+
+        int i = 0;
+        int j = 0;
+
+        while (i < rowSum.size() && j < colSum.size())
+        {
+            result[i][j] = min(rowSum[i], colSum[j]);
+
+            rowSum[i] -= result[i][j];
+            colSum[j] -= result[i][j];
+
+            if (rowSum[i] == 0)
+            {
+                i++;
+            }
+
+            if (colSum[j] == 0)
+            {
+                j++;
+            }
+        }
+
+        return result;
+    }
+};
