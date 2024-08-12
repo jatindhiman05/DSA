98f23e205908c9d96bfdebf44f1b1361945e8e24
jatinn27 <jatin121dhiman@gmail.com>
2024-08-12 20:44:50 +0530

    Kth Largest Element in a Stream

---
+++ 19-HEAPS/4-Kth Largest Element in a Stream/sol.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include <queue>
+using namespace std;
+
+class KthLargest
+{
+    int k;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+public:
+    KthLargest(int k, vector<int> &nums)
+    {
+        this->k = k;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            pq.push(nums[i]);
+        }
+
+        while (pq.size() > k)
+        {
+            pq.pop();
+        }
+    }
+
+    int add(int val)
+    {
+        if (pq.size() < k)
+        {
+            pq.push(val);
+        }
+        else if (val > pq.top())
+        {
+            pq.push(val);
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
